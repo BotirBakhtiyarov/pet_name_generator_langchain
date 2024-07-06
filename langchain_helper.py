8585c1fdafde561ec24ddf172249fea3e6f098a0
botir <botirbakhtiyarovb@gmail.com>
2024-07-07 06:04:22 +0800

    done

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,6 +1,10 @@
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,5 +23,18 @@ def generate_pet_name(animal_type, pet_color):
     return response['text']
     # return response
 
+def langchain_agent():
+    llm = OpenAI(temperature=0.6)
+
+    tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+
+    result = agent.run("What is the average age of a dog? Multiply the age by 3")
+    print(result)
+
 if __name__ == "__main__":
-    print(generate_pet_name("elephant", "white"))
+    # print(generate_pet_name("elephant", "white"))
+    langchain_agent()
